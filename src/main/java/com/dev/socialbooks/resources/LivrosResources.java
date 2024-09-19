eefa3c213875ff5a0741be9e523d38c0720f326f
marcossbento <marcossbento14@gmail.com>
2024-09-19 16:59:11 -0300

    http methods initial config

--- src/main/java/com/dev/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/dev/socialbooks/resources/LivrosResources.java
@@ -2,8 +2,13 @@ package com.dev.socialbooks.resources;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,6 +31,28 @@ public class LivrosResources {
 	
 	@RequestMapping(method = RequestMethod.POST)
 	public void salvar(@RequestBody Livro livro) {
+		livrosRepository.save(livro);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
+		Optional<Livro> livro = livrosRepository.findById(id);
+		
+		if(livro == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(livro);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void deletar(@PathVariable("id") Long id) {
+		livrosRepository.deleteById(id);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public void atualizar(@RequestBody Livro livro, @PathVariable("id") Long id) {
+		livro.setId(id);
 		livrosRepository.save(livro);
 	}
 	
